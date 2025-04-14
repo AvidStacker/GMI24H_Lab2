967a667c45707370136a8d8df8db406322354256
Isak Lagerberg <dev.lagerberg@gmail.com>
2025-04-14 09:58:40 +0200

    Tog bort onödiga kommentarer

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ of a given positive integer n using recursion.
 recursion"""
 
 def fibonacci(n):
-    # Check if n is less than 0
     if n < 0:
         print("Incorrect input")
-    # Check if n is equal to 0
     elif n == 0:
         return 0
-    # Check if n is equal to 1
     elif n == 1:    
         return 1
     else:
